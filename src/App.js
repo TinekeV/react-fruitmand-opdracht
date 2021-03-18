2d2b37e32c284b11c6ffa85937158478c9c64ef8
TinekeV <tinekevlieger@gmail.com>
2021-03-18 15:47:44 +0100

    eerste opzet gemaakt

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+
   return (
     <>
       <h1>Fruitmand bezorgservice</h1>
+      <h3>🍓 Aardbeien</h3>
+
+        <button
+            type="button"
+            onClick={counter - 1}
+        > - </button>
+        <h3>0</h3>
+        <button
+            type="button"
+            onClick={counter + 1}
+        > + </button>
+      <h3>🍌 Bananen</h3>
+
+        <button type="button"> - </button>
+        <h3>0</h3>
+
+        <button type="button"> + </button>
+      <h3>🍏 Appels</h3>
+
+        <button type="button"> - </button>
+        <h3>0</h3>
+        <button type="button"> + </button>
+
+      <h3>🥝 Kiwi's</h3>
+        <button type="button"> - </button>
+        <h3>0</h3>
+        <button type="button"> + </button>
     </>
   );
 }
